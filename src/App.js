2ca23f67ca4404f42ed5120b44d6897f3c6b71c6
Alexander Popov <popov.a.connect@gmail.com>
2024-01-23 18:53:43 +0300

    Footer navbar.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Explore from "./pages/Explore";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import NavBar from "./components/NavBar";
+
 
 function App() {
+
   return (
-    <div>
-      <h1>My App</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/forgotpassword' element={<ForgotPassword />} />
+          <Route path='/profile' element={<SignIn />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/Singin' element={<SignIn />} />
+          <Route path='/Singup' element={<SignUp />} />
+        </Routes>
+        <NavBar />
+      </Router>
+    </>
   );
 }
 
